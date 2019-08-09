f9493350b6c9a080b7c123b3158bd0e048a7ec32
Duc Trinh <ducanhtrinh93@gmail.com>
2019-08-09 12:26:28 +0200

    Stringify messages

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const Sse = require('json-sse')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
 const app = express()
+
+const middleware = cors()
+app.use(middleware)
 
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
 
 const messages = ['Hello World']
-const sse = new Sse(messages)
+const data = JSON.stringify(messages)
+
+const sse = new Sse(data)
 
 app.get('/stream', sse.init)
 
@@ -16,8 +22,12 @@ app.post('/message', (req, res) => {
     const { message } = req.body
 
     messages.push(message)
-    sse.updateInit(messages)
-    sse.send(message)
+
+    const data = JSON.stringify(messages)
+
+    sse.updateInit(data)
+    sse.send(data)
+    
     res.send(message)
 })
 
