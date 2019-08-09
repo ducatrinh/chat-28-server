570018b0b8b4074f049e7d9ce2b5272ea91c58fd
Duc Trinh <ducanhtrinh93@gmail.com>
2019-08-09 11:07:57 +0200

    Add update messages

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ const app = express()
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
 
-const data = 'Hello World'
-const sse = new Sse(data)
+const messages = ['Hello World']
+const sse = new Sse(messages)
 
 app.get('/stream', sse.init)
+
 app.post('/message', (req, res) => {
     const { message } = req.body
+
+    messages.push(message)
+    sse.updateInit(messages)
     sse.send(message)
     res.send(message)
 })
