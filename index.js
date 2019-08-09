f45bbd6192b536b39c9dcc589e28f39cb3f98f21
Duc Trinh <ducanhtrinh93@gmail.com>
2019-08-09 10:42:37 +0200

    Enable sse stream

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express')
-const app = express()
+const Sse = require('json-sse')
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000
+const app = express()
+
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
+
+const data = 'Hello World'
+const sse = new Sse(data)
+
+app.get('/stream', sse.init)
+app.post('/message', (req, res) => {
+    const { message } = req.body
+    sse.send(message)
+    res.send(message)
+})
 
 app.listen(port, () => console.log(`Listening on :${port}`))
